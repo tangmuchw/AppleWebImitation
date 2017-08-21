28b858d4b5a4ca1f24817872f82bb17c2d389940
tangmuche <tangmuchw.@163.com>
2017-08-21 11:21:19 +0800

    为首页页面的商品导航引用swiper插件

--- js/apple.js
+++ js/apple.js
@@ -1,20 +1,20 @@
 // JavaScript Document
-autoplay=true;
+autoplay = true;
 var mySwiper = new Swiper('.swiper-container', {
 	autoplay: 3000,
 	progress: true,
 	speed: 1000,
 	paginationClickable: true,
-	loop: true,
+	loop: 1,
 	onProgressChange: function(swiper) {
-		for (var i = 0; i < swiper.slides.length; i++) {
+		for(var i = 0; i < swiper.slides.length; i++) {
 			var slide = swiper.slides[i];
 			var progress = slide.progress;
 			var translate;
-			if (progress > 0) {
+			if(progress > 0) {
 				translate = progress * 0.9 * swiper.width;
 				scale = 1 - progress * 0.1
-				if (progress > 1) {
+				if(progress > 1) {
 					scale = 0.9
 				}
 				txtPositionX = 0
@@ -28,7 +28,7 @@ var mySwiper = new Swiper('.swiper-container', {
 			}
 
 			var txts = slide.querySelectorAll('.txt');
-			for (var j = 0; j < txts.length; j++) {
+			for(var j = 0; j < txts.length; j++) {
 				swiper.setTransform(txts[j], 'translate3d(' + txtPositionX + ',' + txtPositionY + ',0)');
 			}
 			swiper.setTransform(slide, 'translate3d(' + (translate) + 'px,0,0) scale(' + scale + ')');
@@ -36,30 +36,30 @@ var mySwiper = new Swiper('.swiper-container', {
 		}
 	},
 	onTouchStart: function(swiper) {
-		for (var i = 0; i < swiper.slides.length; i++) {
+		for(var i = 0; i < swiper.slides.length; i++) {
 			swiper.setTransition(swiper.slides[i], 0);
 			var txts = swiper.slides[i].querySelectorAll('.txt');
-			for (var j = 0; j < txts.length; j++) {
+			for(var j = 0; j < txts.length; j++) {
 				swiper.setTransition(txts[j], 0);
 			}
 		}
 	},
 	onSetWrapperTransition: function(swiper, speed) {
-		for (var i = 0; i < swiper.slides.length; i++) {
+		for(var i = 0; i < swiper.slides.length; i++) {
 			swiper.setTransition(swiper.slides[i], speed);
 			var txts = swiper.slides[i].querySelectorAll('.txt');
-			for (var j = 0; j < txts.length; j++) {
+			for(var j = 0; j < txts.length; j++) {
 				swiper.setTransition(txts[j], speed);
 			}
 		}
 	},
 	onSlideChangeStart: function(swiper) {
-		if (autoplay) {
-			if (swiper.activeLoopIndex == 0) {
+		if(autoplay) {
+			if(swiper.activeLoopIndex == 0) {
 				$('.pagination li').removeClass('current');
 				$('.pagination li').eq(0).removeClass('replace');
 			}
-			if (swiper.activeLoopIndex == 1) {
+			if(swiper.activeLoopIndex == 1) {
 				$('.pagination li').eq(0).removeClass('firstCurrent current').addClass('replace')
 			}
 			$('.pagination li').eq(swiper.activeLoopIndex).addClass('current');
@@ -74,13 +74,13 @@ var mySwiper = new Swiper('.swiper-container', {
 	},
 
 	onAutoplayStop: function() {
-		autoplay=false;
+		autoplay = false;
 		$('.autoplay').removeClass('autoplay');
 	},
 })
 
 // Set Z-Indexes
-for (var i = 0; i < mySwiper.slides.length; i++) {
+for(var i = 0; i < mySwiper.slides.length; i++) {
 	mySwiper.slides[i].style.zIndex = i;
 }
 
@@ -94,8 +94,8 @@ $('.arrow-right').on('click', function(e) {
 })
 $(".pagination li").on('touchstart mousedown', function(e) {
 	e.preventDefault()
-	if (autoplay) {
-		autoplay=false;
+	if(autoplay) {
+		autoplay = false;
 		mySwiper.stopAutoplay();
 		$(this).addClass('click');
 	}
