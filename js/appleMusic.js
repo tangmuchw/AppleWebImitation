816711dda6d50ac75e67618c2bdbcf54b31941a6
yxt <294841041@qq.com>
2017-08-24 19:48:42 +0800

    music页面修改

--- js/appleMusic.js
+++ js/appleMusic.js
@@ -1,8 +1,29 @@
 $(function() {
-	$(".card-trigger-clip").click(function() {
-		$(".card-detail").slideToggle(800);
+	$(".trigger1").click(function() {
+		$(".detail1").slideToggle(800);
 	});
-	$(".btn1").click(function(){
+	$(".trigger2").click(function() {
+		$(".detail2").slideToggle(800);
+	});
+	$(".trigger3").click(function() {
+		$(".detail3").slideToggle(800);
+	});
+	$(".trigger4").click(function() {
+		$(".detail4").slideToggle(800);
+	});
+	$(".btn1").click(function() {
 		$(".video1")[0].play();
+	})
+	$(".btn2").click(function() {
+		$(".video2")[0].play();
+	})
+	$(".btn3").click(function() {
+		$(".video3")[0].play();
+	})
+	$(".btn4").click(function() {
+		$(".video4")[0].play();
+	})
+	$(".btn5").click(function() {
+		$(".video5")[0].play();
 	})
 });
